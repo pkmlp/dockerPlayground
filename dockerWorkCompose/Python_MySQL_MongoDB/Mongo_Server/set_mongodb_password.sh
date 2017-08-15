c6649d615989bd0a955ecad7cd5f64ab6a061190
pkmlp <pkmlp@pkmlp.ch>
2017-08-15 13:32:30 +0200

    Update Script

--- dockerWorkCompose/Python_MySQL_MongoDB/Mongo_Server/set_mongodb_password.sh
+++ dockerWorkCompose/Python_MySQL_MongoDB/Mongo_Server/set_mongodb_password.sh
@@ -7,13 +7,13 @@ MONGODB_ADMIN_PASS=$MONGODB_ADMIN_PASS
 # Warten bis MongoDB läuft
 RET=1
 while [[ RET -ne 0 ]]; do
-    sleep 5
+    sleep 3
     mongo admin --eval "help" >/dev/null 2>&1
     RET=$?
 done
  
 # Erstellen admin user
 mongo admin --eval "db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});"
-sleep 3
+
  
 touch /data/db/.mongodb_password_set
