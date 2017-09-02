ca7e302872d712603552e52e0f6180c9b60ef133
pkmlp <pkmlp@pkmlp.ch>
2017-09-02 22:20:24 +0200

    DB-DEV-Env-V2

---
+++ dockerWorkCompose/Python_MySQL_MongoDB_V2/Mongo_Server/set_mongodb_password.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+ 
+# Admin User
+MONGODB_ADMIN_USER=$MONGODB_ADMIN_USER
+MONGODB_ADMIN_PASS=$MONGODB_ADMIN_PASS
+
+# Warten bis MongoDB läuft
+RET=1
+while [[ RET -ne 0 ]]; do
+    sleep 3
+    mongo admin --eval "help" >/dev/null 2>&1
+    RET=$?
+done
+ 
+# Erstellen admin user
+mongo admin --eval "db.createUser({user: '$MONGODB_ADMIN_USER', pwd: '$MONGODB_ADMIN_PASS', roles:[{role:'root',db:'admin'}]});"
+
+ 
+touch /data/db/.mongodb_password_set
